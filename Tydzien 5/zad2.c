4075d6a7b19108608db4fd18c529223b2f576ee4
Kamil Szydlowski <kramil208@gmail.com>
2022-05-16 17:49:10 +0200

    finished tasks 1 and 2

--- Tydzien 5/zad2.c
+++ Tydzien 5/zad2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct Term {
     int hour;
@@ -8,6 +9,10 @@ struct Term {
 };
 
 typedef struct Term Term;
+
+int hour_addition(Term* term1, Term* term2) {
+    return (term2->hour * 60 + term2->minute) - (term1->hour * 60 + term1->minute);
+}
 
 void Term__init(Term* term, int hour, int minute, int duration) {
     term->hour = hour;
@@ -29,6 +34,38 @@ char* Term__toString(Term* term) {
     char* res = malloc(64 * sizeof(char));
     sprintf(res, "\"%d:%d [%d]\"", term->hour, term->minute, term->duration);
     return res;
+}
+
+bool Term__earlierThan(Term* term1, Term *term2) {
+    if(term1->hour < term2->hour) {
+        return true;
+    }
+    else if(term1->hour == term2->hour && term1->minute < term2->minute) {
+        return true;
+    }
+    return false;
+}
+
+bool Term__laterThan(Term* term1, Term *term2) {
+    if(term1->hour > term2->hour) {
+        return true;
+    }
+    else if(term1->hour == term2->hour && term1->minute > term2->minute) {
+        return true;
+    }
+    return false;
+}
+
+bool Term__equals(Term* term1, Term *term2) {
+    return (term1->hour == term2->hour) && (term1->minute == term2->minute) && (term1->duration == term2->duration) ? true : false;
+}
+
+Term* Term__endTerm(Term* term1, Term *term2) {
+    if(Term__earlierThan(term1, term2)) {
+        Term* res_term = Term__create(term1->hour, term1->minute, hour_addition(term1, term2));
+        return res_term;
+    }
+    else return Term__create(-1, -1, -1); 
 }
 
 /*int main(int argc, char** argv) {
--- Tydzien 5/zad2_tests.c
+++ Tydzien 5/zad2_tests.c
@@ -6,6 +6,7 @@ void Term_create_and_init_test() {
     CU_ASSERT_EQUAL(new_term->hour, 14);
     CU_ASSERT_EQUAL(new_term->minute, 30);
     CU_ASSERT_EQUAL(new_term->duration, 90);
+    Term__destroy(new_term);
 }
 
 void Term_toString_test() {
@@ -13,6 +14,53 @@ void Term_toString_test() {
     char* res = Term__toString(new_term);
     CU_ASSERT_STRING_EQUAL(res, "\"14:30 [90]\"");
     free(res);    
+    Term__destroy(new_term);
+}
+
+void Term__earlierThan_test() {
+    Term* term1 = Term__create(13, 30, 90);
+    Term* term2 = Term__create(14, 30, 90);
+    CU_ASSERT_EQUAL(Term__earlierThan(term1, term2), true);
+    CU_ASSERT_EQUAL(Term__earlierThan(term2, term1), false);
+    Term__destroy(term1);
+    Term__destroy(term2);
+}
+
+void Term__laterThan_test() {
+    Term* term1 = Term__create(15, 30, 90);
+    Term* term2 = Term__create(14, 30, 90);
+    CU_ASSERT_EQUAL(Term__laterThan(term1, term2), true);
+    CU_ASSERT_EQUAL(Term__laterThan(term2, term1), false);
+    Term__destroy(term1);
+    Term__destroy(term2);
+}
+
+void Term__equals_test() {
+    Term* term1 = Term__create(15, 30, 90);
+    Term* term2 = Term__create(15, 30, 90);
+    Term* term3 = Term__create(14, 30, 45);
+    CU_ASSERT_EQUAL(Term__equals(term1, term2), true);
+    CU_ASSERT_EQUAL(Term__equals(term1, term3), false);
+    Term__destroy(term1);
+    Term__destroy(term2);
+    Term__destroy(term3);
+}
+
+void Term__endTerm_test() {
+    Term* term1 = Term__create(10, 25, 45);
+    Term* term2 = Term__create(11, 45, 90);
+    Term* res1 = Term__endTerm(term1, term2);
+    CU_ASSERT_EQUAL(res1->hour, 10);
+    CU_ASSERT_EQUAL(res1->minute, 25)
+    CU_ASSERT_EQUAL(res1->duration, 80);
+    Term* res2 = Term__endTerm(term2, term1);
+    CU_ASSERT_EQUAL(res2->hour, -1);
+    CU_ASSERT_EQUAL(res2->minute, -1);
+    CU_ASSERT_EQUAL(res2->duration, -1);
+    Term__destroy(res1);
+    Term__destroy(res2);
+    Term__destroy(term1);
+    Term__destroy(term2);
 }
 
 int main()
@@ -37,6 +85,30 @@ int main()
   }
 
   if (NULL == CU_add_test(pSuite, "Term_toString_test", Term_toString_test))
+  {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if (NULL == CU_add_test(pSuite, "Term__earlierThan_test", Term__earlierThan_test))
+  {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if (NULL == CU_add_test(pSuite, "Term__laterThan_test", Term__laterThan_test))
+  {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if (NULL == CU_add_test(pSuite, "Term__equals_test", Term__equals_test))
+  {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if (NULL == CU_add_test(pSuite, "Term__endTerm_test", Term__endTerm_test))
   {
     CU_cleanup_registry();
     return CU_get_error();
